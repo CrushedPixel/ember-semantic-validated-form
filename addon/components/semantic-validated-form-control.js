3170c1e859338be08fc63026ba75ede8cf0e2c78
CrushedPixel <crushedpixelmaps@gmail.com>
2017-12-03 14:23:15 +0000

    Use own logic to create semantic ui popups instead of ui-popup component, fixing several minor UX issues

--- addon/components/semantic-validated-form-control.js
+++ addon/components/semantic-validated-form-control.js
@@ -1,19 +1,26 @@
 import Ember from 'ember';
 import layout from '../templates/components/semantic-validated-form-control';
+import EmberUUID from 'ember-uuid';
 
 export default Ember.Component.extend({
   layout,
 
   classNameBindings: ['outerClass'],
 
+  outerClass: Ember.computed('displayError', function() {
+    return this.get('displayError') ? 'field error' : 'field';
+  }),
+
   model: null,
   property: null, // must be set initially
 
   showError: false,
   focusLeft: false,
+  wasValid: false, // whether the property was valid at least once
 
-  // the message displayed by the error popup
-  errorPopupMessage: null,
+  hasError: false,
+  popupMessage: undefined,
+  popupId: Ember.computed(EmberUUID.v4),
 
   init() {
     this._super(...arguments);
@@ -26,45 +33,51 @@ export default Ember.Component.extend({
     this.displayError = Ember.computed('showError', 'focusLeft', `model.validations.attrs.${property}.isInvalid`,
       function() {
         const invalid = this.get(`model.validations.attrs.${property}.isInvalid`);
-        return (this.get('showError') || this.get('focusLeft')) && invalid;
+        return this.get('showError') || ((this.get('focusLeft') || this.get('wasValid')) && invalid);
       }
     );
 
-    const updateErrorPopupMessage = function() {
-      const message = this.get(`model.validations.attrs.${property}.message`);
+    const observerFunc = function() {
+      if (this.get(`model.validations.attrs.${property}.isValid`)) {
+        this.set('wasValid', true);
+      }
 
-      // only update the errorPopupMessage if it isn't empty.
-      // this way, the popup doesn't get empty for a split second
-      // before being closed by the hidePopup observer.
-      if (message) {
-        this.set('errorPopupMessage', message);
+      let message = this.get(`model.validations.attrs.${property}.message`);
+
+      const p = this.$('.popup-trigger');
+      if (!p.popup('exists')) {
+        p.popup({
+          popup: `#${this.get('popupId')}`,
+          onShow: () => (this.get('displayError') && this.get('hasError'))
+        });
+      }
+
+      const hasError = message !== undefined;
+      this.set('hasError', hasError);
+
+      if (hasError) {
+        if (this.get('displayError')) {
+          p.popup('show');
+        }
+
+        // only change the displayed popup message if it's not empty,
+        // to avoid the value becoming empty before the popup hiding transition is finished
+        this.set('popupMessage', message);
+      } else {
+        p.popup('hide');
       }
     }.bind(this);
 
-    // invoke it once to set the initial message
-    updateErrorPopupMessage();
-    Ember.addObserver(this, `model.validations.attrs.${property}.message`, updateErrorPopupMessage);
+
+    Ember.addObserver(this, `model.validations.attrs.${property}.message`, observerFunc);
+    Ember.addObserver(this, `model.validations.attrs.${property}.isInvalid`, observerFunc);
+
+    Ember.run.scheduleOnce('afterRender', observerFunc);
   },
-
-  outerClass: Ember.computed('displayError', function() {
-    return this.get('displayError') ? 'field error' : 'field';
-  }),
-
-  hidePopup: Ember.observer('displayError', function() {
-    if (!this.get('displayError')) {
-      this.$('.popup').popup('hide');
-    }
-  }),
 
   actions: {
     onFocusOut() {
       this.set('focusLeft', true);
-    },
-
-    onShowPopup() {
-      // only allow the error message popup to show
-      // if the property is invalid
-      return this.get('displayError');
     }
   }
 
