c84afa9dfc0aa58108566bf4c08fcfd93417104a
CrushedPixel <crushedpixelmaps@gmail.com>
2017-12-03 17:01:05 +0000

    Remove div wrapper around submit button to allow for inline buttons

--- addon/components/semantic-validated-form-button.js
+++ addon/components/semantic-validated-form-button.js
@@ -2,5 +2,24 @@ import Ember from 'ember';
 import layout from '../templates/components/semantic-validated-form-button';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+  tagName: 'button',
+
+  attributeBindings: ['type'],
+  type: 'button',
+
+  classNames: 'ui button',
+  classNameBindings: ['displayClasses'],
+
+  displayClasses: Ember.computed('enabled', function() {
+    if (this.get('enabled')) {
+      return 'positive';
+    } else {
+      return 'basic negative';
+    }
+  }),
+
+  click: function() {
+    this.get('submit')()
+  }
 });
